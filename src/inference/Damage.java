d7140621f0ac6586bf7030b9a452d7582e694b35
Marcus Matic <s3703981@student.rmit.edu.au>
2021-11-21 06:27:02 +1100

    Added type match ups and class to represent type matchups

---
+++ src/inference/Damage.java
@@ -0,0 +1,11 @@
+package inference;
+
+public class Damage {
+    Type defendingType;
+    double damage;
+    
+    public Damage(Type defendingType, double damage){
+        this.defendingType = defendingType;
+        this.damage = damage;
+    }
+}
